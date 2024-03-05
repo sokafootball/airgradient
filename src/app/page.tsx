cf57dadf529ed4cca0195712c954d346139f945d
Laurent Capello <laurent.capello@nttdata.com>
2024-03-05 10:41:58 +0100

    feat: add test chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,36 @@
 import { useGetLocationCurrentMeasuresQuery } from '../../api';
 import { LOCATION_ID } from '../../constants';
 import styles from './page.module.scss';
+import { BarChart, BarChartProps } from '@mui/x-charts/BarChart';
+import { createTheme } from '@mui/material';
+import { lime, purple } from '@mui/material/colors';
 
 const Home = () => {
-  const response = useGetLocationCurrentMeasuresQuery({
+  const { data } = useGetLocationCurrentMeasuresQuery({
     locationId: LOCATION_ID,
   });
-  return <main className={styles.main}>test</main>;
+  const theme = createTheme({
+    palette: {
+      primary: lime,
+      secondary: purple,
+    },
+  });
+
+  return (
+    <main className={styles.main}>
+      <BarChart
+        series={[
+          { data: [35, 44, 24, 34] },
+          { data: [51, 6, 49, 30] },
+          { data: [15, 25, 30, 50] },
+          { data: [60, 50, 15, 25] },
+        ]}
+        height={290}
+        xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
+        margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+      />
+    </main>
+  );
 };
 
 export default Home;
